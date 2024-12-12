072930eda9441440eea7bcb594bd38630d9044e2
TomO256 <github@whiskey.aleeas.com>
2024-12-12 06:19:53 +0000

    Added some ;

--- src/qtApp/persistent-pollutants.cpp
+++ src/qtApp/persistent-pollutants.cpp
@@ -233,13 +233,13 @@ void PersistentPollutants::openCSV()
 
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     axisX->append(categories);
-    axisX->setTitleText("Timestamp")
+    axisX->setTitleText("Timestamp");
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
 
     QValueAxis *axisY = new QValueAxis();
     axisY->setRange(0, 20);
-    axisY->setTitleText("Persistant Pollutant Level")
+    axisY->setTitleText("Persistant Pollutant Level");
     chart->addAxis(axisY, Qt::AlignLeft);
     series->attachAxis(axisY);
 
